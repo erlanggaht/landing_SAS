6b09cd7e82c8e586f1bb6c0293896a4f50dd2e72
erlanggaht <erlanggahidayat.md@gmail.com>
2023-06-24 21:07:15 +0700

    fix

--- src/app/config/url_host.tsx
+++ src/app/config/url_host.tsx
@@ -1,2 +1,2 @@
-export const URL_HOST_API = 'https://sinarasih-backend.vercel.app/' 
-export const URL_HOST_FRONT = 'https://sinarasih.vercel.app/'
+export const URL_HOST_API = 'https://sinarasih-backend.vercel.app' 
+export const URL_HOST_FRONT = 'https://sinarasih.vercel.app'
