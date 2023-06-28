d1dd4d73d2731758bb1f8440dc426da2a34f1b0f
erlanggaht <erlanggahidayat.md@gmail.com>
2023-06-28 19:46:12 +0700

    oeky

--- src/app/components/atoms/line_scroll.tsx
+++ src/app/components/atoms/line_scroll.tsx
@@ -13,12 +13,14 @@ export default function LineScroll({props} : PropsLineScroll) {
   const {width} = props
 
   // Scroll Follow
-  window.addEventListener("scroll", (event) => {
-    let a = document.body.getBoundingClientRect()
-    const b = Math.abs(a.y - 8) * a.height / a.height / 2
- 
-    document.querySelector('#lineScroll')?.setAttribute("style",`width:${b}px;transition:.1s all`)
- });
+  if(typeof window !== 'undefined' ) {
+    window.addEventListener("scroll", (event) => {
+      let a = document.body.getBoundingClientRect()
+      const b = Math.abs(a.y - 8) * a.height / a.height / 2
+   
+      document.querySelector('#lineScroll')?.setAttribute("style",`width:${b}px;transition:.1s all`)
+   })
+  }
  
 
   return (
