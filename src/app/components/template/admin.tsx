891f25a13944ec0fd6f7b1f5044c9b7b24a30aec
erlanggaht <erlanggahidayat.md@gmail.com>
2023-07-09 11:38:08 +0700

    okeh

--- src/app/components/template/admin.tsx
+++ src/app/components/template/admin.tsx
@@ -181,7 +181,7 @@ export default function AdminPage() {
 
         if(rejectState) {
             dispatch(getAdmin())
-            router.push('pages/admin/login')
+            router.push('admin/login')
         }
         
     }, [dispatch,rejectState])
